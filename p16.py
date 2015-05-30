03281ab98690cbf3b351c70d1780c3e603808c66
Timothy Kang <timothykang01@gmail.com>
2015-05-29 19:44:37 -0700

    Finished p16 in one try

---
+++ p16.py
@@ -0,0 +1,14 @@
+import math
+import time
+
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# What is the sum of the digits of the number 2^1000?
+
+def fxn16(x,y):
+	start = time.clock()
+	number = str(x**y)
+	total = 0 
+	for val in number:
+		total += int(val)
+
+	print(str(total) + " in " + str(time.clock() - start) + " seconds. ")
